9400d13e00403a0664daea87ed06a1c2023a5b8e
Sunil Unka <sunil.unka@gmail.com>
2014-07-31 17:48:56 +0000

    Completed basic form and POST functionality

--- app/actions_routes/review.rb
+++ app/actions_routes/review.rb
@@ -1,62 +1,55 @@
-# get '/artist/:id/event/:id/review' do
-#   erb :'artist/event/review'
-# end
-
 
 get '/event/review/new' do
-  # @event = Event.new
+  @review = Review.new
   erb :'event/review/new'
 end
 
 post '/event/review/new' do
   
-  @date = params[:date]
-  @event = params[:event_city].downcase
-  @artist = params[:artist].downcase
-
-  find_artist = Artist.find_by name: @artist
-  find_event = Event.where("date = ? AND (city_name LIKE ?)", @date, @event)
-
+  find_artist = Artist.find_by name: params[:artist]
+  find_event = Event.find_by date: params[:date], city_name: params[:city_name]
 
   
-  if find_artist && find_event.first
-    @review = Review.new(
-      date: find_event.date,
-      event_id: find_event.first.id,
-      # artist_id: find_artist.id
-    )
-  elsif find_artist
-    @event = Event.new(
-        date: @date,
-        city_name: @event,
-        artist_id: find_artist.first.id
-    )  
+  if find_artist
+    if find_event
+      @review = Review.create(
+        event_id: find_event.id,
+        review: params[:review]
+      )
+      redirect "/artist/#{find_artist.id}"
 
-    @review = Review.new(
-      date: @date,
-      event_id: Event.last.id
-      # artist_id: find_artist.first.id,
-    )
+    else
+      @event = Event.create(
+        date: params[:date],
+        city_name: params[:city_name],
+        artist_id: find_artist.id
+      )  
 
+      @review = Review.create(
+        event_id: @event.id,
+        review: params[:review]
+      )
 
-  else 
-    @new_artist = Artist.new(
-      name: @artist
+      redirect "/artist/#{find_artist.id}"
+
+    end
+
+  else
+    @artist = Artist.create(
+      name: params[:artist]
     ) 
 
-    @event = Event.new(
-      date: @date,
-      city_name: @event
-      artist_id: Artist.last
+    @event = Event.create(
+      date: params[:date],
+      city_name: params[:city_name],
+      artist_id: @artist.id
     )
 
-    @review = Review.new(
-      date: @date,
-      event_id: Event.last.id
-      # artist_id: Artist.last.id
+    @review = Review.create(
+      event_id: @event.id,
+      review: params[:review]
     )
+
+    redirect "/artist/#{@artist.id}"
   end
-
-  redirect "/artist/#{find_artist.id}"
-  # erb :'artist/event/review/new
 end
