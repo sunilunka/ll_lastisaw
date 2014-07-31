c1fd4c010a9229272b187b04c217794c8f9c9281
Sunil Unka <sunil.unka@gmail.com>
2014-07-31 04:31:03 +0000

    Modified CSS files and review.rb POST actions.

--- app/actions_routes/review.rb
+++ app/actions_routes/review.rb
@@ -10,12 +10,53 @@ end
 
 post '/event/review/new' do
   
-  @event = Event.new(
-    date: params[:date],
-    event_id: params [:event_city],
-    artist_id: params[:artist_id]
+  @date = params[:date]
+  @event = params[:event_city].downcase
+  @artist = params[:artist].downcase
+
+  find_artist = Artist.find_by name: @artist
+  find_event = Event.where("date = ? AND (city_name LIKE ?)", @date, @event)
+
+
+  
+  if find_artist && find_event.first
+    @review = Review.new(
+      date: find_event.date,
+      event_id: find_event.first.id,
+      # artist_id: find_artist.id
+    )
+  elsif find_artist
+    @event = Event.new(
+        date: @date,
+        city_name: @event,
+        artist_id: find_artist.first.id
+    )  
+
+    @review = Review.new(
+      date: @date,
+      event_id: Event.last.id
+      # artist_id: find_artist.first.id,
     )
 
-  redirect '/artist/:id'
+
+  else 
+    @new_artist = Artist.new(
+      name: @artist
+    ) 
+
+    @event = Event.new(
+      date: @date,
+      city_name: @event
+      artist_id: Artist.last
+    )
+
+    @review = Review.new(
+      date: @date,
+      event_id: Event.last.id
+      # artist_id: Artist.last.id
+    )
+  end
+
+  redirect "/artist/#{find_artist.id}"
   # erb :'artist/event/review/new
 end
