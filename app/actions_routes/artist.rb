7b5e3e1635774a46e6db4c3ad64b5c7416621255
Sunil Unka <sunil.unka@gmail.com>
2014-08-02 21:54:51 +0000

    Completed Homepage artist search functinality and styling.

--- app/actions_routes/artist.rb
+++ app/actions_routes/artist.rb
@@ -1,11 +1,11 @@
+get '/artist/search' do
+  @artist = Artist.find_by(name: params[:artist_search_term])
+  redirect "/artist/#{@artist.id}"
+end
+
 get '/artist/:id' do
     @artist = Artist.find(params[:id])
     @events = @artist.events.order(date: :desc)
     @tags = Instagram.tag_recent_media("#{@artist.name.downcase.gsub(/[\W_]/,"")}")
     erb :'artist/artist'
-end
-
-get '/artist/search/:artist_search_term' do
-  @artist = Artist.find_by(name: params[:artist_search_term])
-  redirect "/artist/#{@artist.id}"
 end
